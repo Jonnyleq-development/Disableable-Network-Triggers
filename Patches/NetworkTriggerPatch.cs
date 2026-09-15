82d6b23852abc0877b7d03fd7655114c3abc3fb1
Jonnyleq-development <leifwinters1@proton.me>
2026-09-15 00:03:57 -0500

    Update NetworkTriggerPatch.cs

--- Patches/NetworkTriggerPatch.cs
+++ Patches/NetworkTriggerPatch.cs
@@ -4,11 +4,10 @@ using HarmonyLib;
 namespace DisableNetworkTriggers
 {
     [HarmonyPatch(typeof(GorillaNetworkJoinTrigger), nameof(GorillaNetworkJoinTrigger.OnBoxTriggered))]
-    public class NetworkTriggerPatch
+    public static class NetworkTriggerPatch
     {
-        public static bool enabled;
+        public static bool disabled;
 
-        public static bool Prefix() =>
-            !enabled;
+        static bool Prefix() => !disabled;
     }
 }
